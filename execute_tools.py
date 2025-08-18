ada0fe8a717c02aca585a9836ca720c8f94a9cff
Atharva Anand Tembhurnikar <tembhurnikaraa@rknec.edu>
2025-08-18 13:41:27 +0530

    Update execute_tools.py

--- execute_tools.py
+++ execute_tools.py
@@ -11,7 +11,7 @@ tavily_tool = TavilySearch(
     topic="news",
     # Additional parameters for better search results
     search_depth="advanced",
-    time_range="none"
+   # time_range="none"
 )
 
 def safe_json_serialize(obj):
